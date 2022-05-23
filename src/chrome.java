b7e84cfac4361e263377973e3d7f8876388c5832
unknown <mindsdet135@iiht.tech>
2022-05-23 13:48:15 +0530

    Testcase

---
+++ src/chrome.java
@@ -0,0 +1,16 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mindsdet135\\Desktop\\chromedriver.exe");
+    WebDriver driver=new ChromeDriver();
+    driver.get("http://amazon.com");
+	}
+
+}
+
